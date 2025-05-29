9962f82016cb37058df383674f99f6afa2a71e27
SuiWolfPack <barn_knock03@icloud.com>
2025-05-29 22:24:45 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/wolf-dao/' // 👈 Important for GitHub Codespaces and Pages
+  base: '/wolf-dao/', // Important for GitHub Pages and Codespaces subpath
 });
