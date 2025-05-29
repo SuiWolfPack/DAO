d670a5ec9fcfc651542cb33561ad9f18f77cb105
SuiWolfPack <barn_knock03@icloud.com>
2025-05-29 22:23:27 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { WalletProvider } from '@suiet/wallet-kit';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <WalletProvider>
-      <App />
-    </WalletProvider>
+    <BrowserRouter basename="/wolf-dao">
+      <WalletProvider>
+        <App />
+      </WalletProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
